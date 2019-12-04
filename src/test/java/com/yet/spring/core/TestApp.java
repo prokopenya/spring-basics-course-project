84b1c4a8f9834db7b148ffd278c0b7de83a99c7a
prokopenya <igorprokopenya@gmail.com>
2019-12-04 15:22:11 +0300

    pognali

---
+++ src/test/java/com/yet/spring/core/TestApp.java
@@ -0,0 +1,33 @@
+package com.yet.spring.core;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class TestApp {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void testLogEvent() {
+        App.main(null);
+        String msg = outContent.toString();
+        Assert.assertFalse(msg.contains("1"));
+        Assert.assertTrue(msg.contains("Igor"));
+    }
+
+}
