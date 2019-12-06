d55b9497225e38ab586901e24811eef9334d4a63
prokopenya <igorprokopenya@gmail.com>
2019-12-06 11:43:10 +0300

    combined loggers

--- src/main/java/com/yet/spring/core/App.java
+++ src/main/java/com/yet/spring/core/App.java
@@ -3,14 +3,17 @@ package com.yet.spring.core;
 import com.yet.spring.core.beans.Client;
 import com.yet.spring.core.beans.Event;
 import com.yet.spring.core.beans.EventLogger;
+import com.yet.spring.core.beans.EventType;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Map;
 
 public class App {
 
     private Client client;
-
-    private EventLogger eventLogger;
+    private EventLogger defaultLogger;
+    private Map<EventType, EventLogger> loggers;
 
     public static void main(String[] args) {
 
@@ -18,23 +21,34 @@ public class App {
         App app = (App) ctx.getBean("app");
 
         Event event = ctx.getBean(Event.class);
-        app.logEvent(event, "Some event for 1");
+        app.logEvent(EventType.INFO, event, "Some event for 1");
 
         event = ctx.getBean(Event.class);
-        app.logEvent(event, "Some event for 2");
+        app.logEvent(EventType.ERROR, event, "Some event for 2");
+
+        event = ctx.getBean(Event.class);
+        app.logEvent(null, event, "Some event for 3");
 
         ctx.close();
     }
 
-    public App(Client client, EventLogger eventLogger) {
+    public App(Client client, EventLogger eventLogger, Map<EventType, EventLogger> loggers) {
         super();
         this.client = client;
-        this.eventLogger = eventLogger;
+        this.defaultLogger = eventLogger;
+        this.loggers = loggers;
+
     }
 
-    private void logEvent(Event event, String msg) {
+    private void logEvent(EventType eventType, Event event, String msg) {
         String message = msg.replaceAll(client.getId(), client.getFullName());
         event.setMsg(message);
-        eventLogger.logEvent(event);
+
+        EventLogger logger = loggers.get(eventType);
+        if (logger == null) {
+            logger = defaultLogger;
+        }
+
+        logger.logEvent(event);
     }
 }
--- src/main/java/com/yet/spring/core/beans/Client.java
+++ src/main/java/com/yet/spring/core/beans/Client.java
@@ -3,8 +3,8 @@ package com.yet.spring.core.beans;
 public class Client {
 
     private String id;
-
     private String fullName;
+    private String greeting;
 
     public Client(String id, String fullName) {
         super();
@@ -26,6 +26,14 @@ public class Client {
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
     }
 
 }
